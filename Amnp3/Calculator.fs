07aa7c1a2ba85719da7d59fbd2627ae8dfcbd054
gaboe <gabrielecegi@gmail.com>
2019-01-05 16:41:12 +0100

    Add project files.

---
+++ Amnp3/Calculator.fs
@@ -0,0 +1,43 @@
+﻿module Calculator
+open FSharp.Data
+open MathNet.Numerics.LinearAlgebra
+
+type DataInput = CsvProvider<"./input.csv">
+
+type Parameters = { y1: decimal; y2: decimal; u1: decimal; u2: decimal}
+
+let toMatrix p =
+    let m = matrix [[ float p.y1; float p.y2; float p.u1; float p.u2 ]]
+    m
+
+type RowList = DataInput.Row list
+
+let negate (n: decimal) = System.Decimal.Negate n
+
+let rowOnIdexOrDefault i rows = 
+    match List.tryItem i rows with
+        | Some x -> x 
+        | None -> DataInput.Row(0M, 0M)
+
+let getFi index (rows: DataInput.Row list)  = 
+    let _1 = rowOnIdexOrDefault (index - 1) rows
+    let _2 = rowOnIdexOrDefault (index - 2) rows
+    { y1 = negate _1.Y; y2 = negate _2.Y; u1 = _1.U; u2 = _2.U }
+
+let getPredictionError y fi prevParams = 
+    //let change = negate(prevParams.y1 * fi.y1) + negate(prevParams.y2 * fi.y2) 
+    //                + (prevParams.u1 * fi.u1) + (prevParams.u2 + fi.u2) |> float
+
+    let change = ((toMatrix fi)).Multiply((toMatrix prevParams).Transpose()).Determinant()
+    let error = (float y) + change;
+    error
+
+let inputData = DataInput.Load("./input.csv")
+
+let rows = Seq.toList inputData.Rows
+
+let _zeroFi = getFi 0 rows 
+
+let sucasnaMatica = {y1 = 2M; y2 = 2M; u1 = 2M; u2 = 2M};
+let predchMatica = {y1 = 1M; y2 = 1M; u1 = 1M; u2 = 1M};
+let _error = getPredictionError 2 sucasnaMatica predchMatica
---
+++ Amnp3/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+// Poznámky z výuky
+// Fí^T(k)=[-y(k-1)  -y(k-2)  u(k-1)  u(k-2)]
+// e(k)=y(k)-Fí^T(k)*Baňka(k-1)
+// c(k)=c(k-1)-( c(k-1)*Fi(k)*FiT(k)*c(k-1) )/( 1+FiT(k)*c(k-1)*Fi(k) )
+// Baňka(k)=Baňka(k-1)+c(k)*Fí(k)*e(k)
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
